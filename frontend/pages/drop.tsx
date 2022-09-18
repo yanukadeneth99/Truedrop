315f56d59dc0c23247a010c18de15c767f6f0a14
yanukadeneth99 <yanukadeneth99@gmail.com>
2022-09-18 18:40:41 +0530

    Squashed commit of the following:
    commit 83f8fcd0bad72609d0308b5feab068521b7220ea
    Author: yanukadeneth99 <yanukadeneth99@gmail.com>
    Date:   Sun Sep 18 18:38:40 2022 +0530
    
        Setup wallet connection using thirdweb
    
    commit df9e1c402ffcd85d5cd04a2a93fa2d40cb1ccf11
    Author: yanukadeneth99 <yanukadeneth99@gmail.com>
    Date:   Sun Sep 18 17:21:57 2022 +0530
    
        Edit content

--- frontend/pages/drop.tsx
+++ frontend/pages/drop.tsx
@@ -2,19 +2,24 @@ import { GetStaticProps } from "next";
 import Footer from "../components/Footer";
 import Form from "../components/Form";
 import Navbar from "../components/Navbar";
+import { NextSeo } from "next-seo";
 
-const drop = () => {
+const Drop = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <section className="">
-        <h2 className="text-3xl text-center my-10 font-semibold">
-          Create Drop Now...
-        </h2>
-        <Form />
-      </section>
-      <Footer />
-    </div>
+    <>
+      <NextSeo title="Drop | Truedrop" description="Create your Souldrop now" />
+      ;
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <section className="">
+          <h2 className="text-3xl text-center my-10 font-semibold">
+            Create Drop Now...
+          </h2>
+          <Form />
+        </section>
+        <Footer />
+      </div>
+    </>
   );
 };
 
@@ -26,4 +31,4 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
   };
 };
 
-export default drop;
+export default Drop;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,24 +1,29 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import { NextSeo } from "next-seo";
+import { motion } from "framer-motion";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
-import styles from "../styles/Home.module.css";
-import { motion } from "framer-motion";
 
 const Home: NextPage = () => {
   return (
-    <div className="min-h-screen flex flex-col ">
-      <Navbar />
-      <motion.div
-        animate={{ y: [0, -130, 0] }}
-        transition={{ duration: 2, delay: 1.5 }}
-      >
-        <Hero />
-      </motion.div>
-      <Footer />
-    </div>
+    <>
+      <NextSeo
+        title="Truedrop"
+        description="Easy souldrop intialiser for your projects!"
+      />
+
+      <div className="min-h-screen flex flex-col ">
+        <Navbar />
+        <motion.div
+          animate={{ y: [0, -130, 0] }}
+          transition={{ duration: 2, delay: 1.5 }}
+        >
+          <Hero />
+        </motion.div>
+        <Footer />
+      </div>
+    </>
   );
 };
 
